9777521ad5f8fd94d4dd5ae963f6cf08d2c5d323
Brian Spendolini <69256666+JetterMcTedder@users.noreply.github.com>
2024-02-05 07:02:51 -1000

    Create create_aiuser.sql

---
+++ data/scripts/create_aiuser.sql
@@ -0,0 +1,26 @@
+# In master
+CREATE LOGIN aiuser WITH PASSWORD = '#######';
+GO
+
+
+# In DEMO DB
+
+create schema aidemo;
+
+CREATE USER [aidemo] FOR LOGIN [aiuser] WITH DEFAULT_SCHEMA=[dbo]
+GO
+ALTER AUTHORIZATION ON SCHEMA::[aidemo] TO [aidemo]
+GO
+USE [RedisAiDb]
+GO
+EXEC sp_addrolemember N'db_datareader', N'aidemo'
+GO
+USE [RedisAiDb]
+GO
+EXEC sp_addrolemember N'db_datawriter', N'aidemo'
+GO
+
+grant CREATE TABLE to aidemo;
+grant CREATE VIEW to aidemo;
+grant CREATE FUNCTION to aidemo;
+grant CREATE PROCEDURE to aidemo;
